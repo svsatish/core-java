e9c32bbf808f6fa9ae8c5d8462b873217516031f
satishs <satish.saripella@sap.com>
2024-04-20 06:51:52 -0400

    comments

---
+++ comments.java
@@ -0,0 +1,22 @@
+// Comments are used to explain code and make it more readable
+// Comments are ignored by the compiler
+// Comments can be single-line or multi-line
+// Comments can be placed almost anywhere in the code
+// Comments can be used to prevent execution when testing code
+public class comments {
+
+    // Single-line comment
+
+    /* Multi-line
+    comment
+    */
+
+    /**
+     * Javadoc comment
+     *
+     * @param args
+     */
+
+
+
+}
